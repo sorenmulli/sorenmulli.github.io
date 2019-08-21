b21d0e8252739532d323b635f21f339b81d04bde
Asgerius den Store <s183912@student.dtu.dk>
2019-08-21 21:53:41 +0200

    opdateret deploy

--- src/frontend/deploy.sh
+++ src/frontend/deploy.sh
@@ -1,16 +1,20 @@
+#!/bin/bash
+shopt -s extglob
+
 # Sikrer, at wd er øverst i Angular-applikationen
 cd "$(dirname "$0")"
 
 # Bygger og rykker til øverst i git-repoet
-ng build --prod
+# ng build --prod
+rm !(.git|.gitignore|src|docs)
 yes | cp -rf dist/frontend/* ../..
 rm -rf dist
 
 # Pusher til git
-git add -A
-git commit -m"$1"
-git pull
-git push
+# git add -A
+# git commit -m"$1"
+# git pull
+# git push
 
 
 
