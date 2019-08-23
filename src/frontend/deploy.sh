369a7a24eb361b6e2e1e28a644304004ad763fed
Asgerius den Store <s183912@student.dtu.dk>
2019-08-23 14:17:17 +0200

    {:-'Nyt build'}

--- src/frontend/deploy.sh
+++ src/frontend/deploy.sh
@@ -4,18 +4,25 @@ shopt -s extglob
 # Sikrer, at wd er øverst i Angular-applikationen
 cd "$(dirname "$0")/../.."
 
-# Bygger og rykker til øverst i git-repoet
-
+# Rydder op i dir
 rm -rf !(.git|.gitignore|src|docs)
 
+# Fjerner cache >:O
 cd src/frontend
-# ng build --prod
-yes | cp -rf dist/frontend/* ../..
-# rm -rf dist
+rm -rf node_modules/.cache
 
-# Pusher til git
+# # Til gh-pages branch
+# ng deploy
+
+# Til master branch
+# Bygger og rykker til øverst i git-repoet
+ng build --prod
+yes | cp -rf dist/frontend/* ../..
+rm -rf dist
+
+# # Pusher til git
 git add -A
-git commit -m"$1"
+git commit -m"{$1:-'Nyt build'}"
 git pull
 git push
 
